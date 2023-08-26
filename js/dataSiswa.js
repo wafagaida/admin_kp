149c6052e426aa9223cd7c409850f7fe6ec896da
wafagaida <wafaghaida.95@gmail.com>
2023-08-26 20:00:41 +0700

    yeayy

--- js/dataSiswa.js
+++ js/dataSiswa.js
@@ -20,16 +20,16 @@ function createTableRow(post, index) {
             <td>${post.alamat ? post.alamat : '-'}</td>
             <td>${post.no_tlp ? post.no_tlp : '-'}</td>
             <td>
-                <button type="button" class="btn btn-success btn-icon-split btn-sm" data-toggle="modal" data-target="#editModal" onclick="openEditModal('${post.nis}')">
+                <button type="button" id="editButton" class="btn btn-success btn-icon-split btn-sm" data-toggle="modal" data-target="#editModal">
                     <span class="icon text-white">
                         <i class="fas fa-edit"></i>
                     </span>
                 </button>
-                <a href="#" class="btn btn-danger btn-icon-split btn-sm" onclick="deleteData('${post.nis}')">
+                <button type="button" class="btn btn-danger btn-icon-split btn-sm" onclick="deleteData('${post.nis}')">
                     <span class="icon text-white">
                         <i class="fas fa-trash"></i>
                     </span>
-                </a>
+                </button>
             </td>
         </tr>
     `;
@@ -69,94 +69,259 @@ function addData() {
     })
         .then(response => {
             if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
+                throw new Error(`HTTP error! Status: ${response.url}`);
             }
             return response.json();
         })
         .then(data => {
             if (data.success === true) {
-                // Tampilkan pesan sukses
+                console.log('Success:', data);
+
                 const successMessage = document.createElement('div');
                 successMessage.classList.add('alert', 'alert-success');
                 successMessage.textContent = "Data berhasil ditambahkan!";
                 modalBody.appendChild(successMessage);
 
-                // Refresh the data
                 getPosts();
 
-                // Hide the modal after a short delay
                 setTimeout(() => {
                     $('#addModal').modal('hide');
-                }, 1000);
+                    successMessage.remove();
+                }, 1200);
+            } else {
+                console.error("Gagal menambah data.");
 
-                // Remove the success message after a delay
+                const successMessage = document.createElement('div');
+                successMessage.classList.add('alert', 'alert-danger');
+                successMessage.textContent = "Data gagal ditambahkan!";
+                modalBody.appendChild(successMessage);
+
                 setTimeout(() => {
                     successMessage.remove();
-                }, 3000);
-            } else {
-                console.error("Failed to add data.");
+                }, 1200);
             }
         })
         .catch(error => {
             console.error("Error adding data:", error);
-        });
-}
 
-function openEditModal(nis) {
-    fetch(`http://127.0.0.1:8000/api/posts/${nis}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.success === true) {
-                const editModal = document.getElementById('editModal');
-                fillEditForm(data.data); // Fill the edit form with data
-                $(editModal).modal('show'); // Show the edit modal
-            } else {
-                console.error("Failed to fetch data for edit.");
-            }
-        })
-        .catch(error => {
-            console.error("Error fetching data for edit:", error);
-        });
-}
+            // const errorMessage = error.message || "Error menambah data!";
+            const errorMessage = document.createElement('div');
+            errorMessage.classList.add('alert', 'alert-danger');
+            errorMessage.textContent = "Error menambah data!";
+            modalBody.appendChild(errorMessage);
 
-function fillEditForm(data) {
-    const editForm = document.getElementById('editForm');
-    // Fill the form fields with data properties
-    document.getElementById('nis').value = data.nis;
-    document.getElementById('nama').value = data.nama;
-    document.getElementById('username').value = data.username;
-    // ... and so on for other fields
+            setTimeout(() => {
+                errorMessage.remove();
+            }, 1200);
+        });
 }
 
 function editData() {
+
     const editForm = document.getElementById('editForm');
     const formData = new FormData(editForm);
+    const modalBody = document.querySelector('.modal-body');
 
-    fetch("http://127.0.0.1:8000/api/posts/update", {
-        method: "POST",
-        body: formData,
+    const nis = formData.get('nis');
+
+    fetch(`http://127.0.0.1:8000/api/posts/${nis}`, {
+        method: 'PUT',
+        body: JSON.stringify(Object.fromEntries(formData)),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.url}`);
+            }
+            return response.json();
+        })
         .then(data => {
             if (data.success === true) {
-                const editModal = document.getElementById('editModal');
-                $(editModal).modal('hide'); // Close the edit modal
-                getPosts(); // Refresh the data
+                console.log('Success:', data);
 
-                // Display update success message, similar to the add process
-                // ...
+                const successMessage = document.createElement('div');
+                successMessage.classList.add('alert', 'alert-success');
+                successMessage.textContent = "Data berhasil diubah!";
+                modalBody.appendChild(successMessage);
+
+                getPosts();
+
+                setTimeout(() => {
+                    $('#editModal').modal('hide');
+                    successMessage.remove();
+                }, 1200);
 
             } else {
-                console.error("Failed to update data.");
+                console.error("Gagal mengedit data.");
+
+                const successMessage = document.createElement('div');
+                successMessage.classList.add('alert', 'alert-danger');
+                successMessage.textContent = "Data gagal ditambahkan!";
+                modalBody.appendChild(successMessage);
+
+                setTimeout(() => {
+                    successMessage.remove();
+                }, 1200);
             }
         })
         .catch(error => {
-            console.error("Error updating data:", error);
+            console.error('Error editing data:', error);
+
+            const errorMessage = document.createElement('div');
+            errorMessage.classList.add('alert', 'alert-danger');
+            errorMessage.textContent = "Error mengedit data!";
+            modalBody.appendChild(errorMessage);
+
+            setTimeout(() => {
+                errorMessage.remove();
+            }, 1200);
         });
 }
 
+
+// function openEditModal(nis) {
+//     fetch(`http://127.0.0.1:8000/api/posts/${nis}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             if (data.success === true) {
+//                 const editModal = document.getElementById('editModal');
+//                 fillEditForm(data.data); // Memanggil fungsi fillEditForm dengan data yang diterima
+//                 $(editModal).modal('show'); // Menampilkan modal edit
+//             } else {
+//                 console.error("Failed to fetch data for edit.");
+//             }
+//         })
+//         .catch(error => {
+//             console.error("Error fetching data for edit:", error);
+//         });
+// }
+
+// function fillEditForm(data) {
+//     const editForm = document.getElementById('editForm');
+//     document.getElementById('nis').value = data.nis;
+//     document.getElementById('nama').value = data.nama;
+//     document.getElementById('jenis_kelamin').value = data.jenis_kelamin;
+//     document.getElementById('tingkat').value = data.tingkat;
+//     document.getElementById('jurusan').value = data.jurusan;
+//     document.getElementById('kd_kelas').value = data.kd_kelas;
+//     document.getElementById('tahun_masuk').value = data.tahun_masuk;
+//     document.getElementById('nik').value = data.nik;
+//     document.getElementById('tanggal_lahir').value = data.tanggal_lahir;
+//     document.getElementById('alamat').value = data.alamat;
+//     document.getElementById('no_tlp').value = data.no_tlp;
+//     document.getElementById('username').value = data.username;
+//     document.getElementById('password').value = data.password;
+// }
+
+// function editData() {
+//     const editForm = document.getElementById('editForm');
+//     const formData = new FormData(editForm);
+
+//     fetch(`http://127.0.0.1:8000/api/posts` ,{
+//         method: "POST",
+//         body: formData,
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             if (data.success === true) {
+//                 const editModal = document.getElementById('editModal');
+//                 $(editModal).modal('hide'); // Close the edit modal
+//                 getPosts(); // Refresh the data
+
+//                 // Display update success message, similar to the add process
+//                 // ...
+
+//             } else {
+//                 console.error("Failed to update data.");
+//             }
+//         })
+//         .catch(error => {
+//             console.error("Error updating data:", error);
+//         });
+// }
+
+// function populateEditForm(data) {
+//     document.getElementById('nis').value = data.nis;
+//     document.getElementById('nama').value = data.nama;
+//     document.getElementById('jenis_kelamin').value = data.jenis_kelamin;
+//     document.getElementById('tingkat').value = data.tingkat;
+//     document.getElementById('jurusan').value = data.jurusan;
+//     document.getElementById('kd_kelas').value = data.kd_kelas;
+//     document.getElementById('tahun_masuk').value = data.tahun_masuk;
+//     document.getElementById('nik').value = data.nik;
+//     document.getElementById('tanggal_lahir').value = data.tanggal_lahir;
+//     document.getElementById('alamat').value = data.alamat;
+//     document.getElementById('no_tlp').value = data.no_tlp;
+//     document.getElementById('username').value = data.username;
+//     document.getElementById('password').value = data.password;
+// }
+
+// function fetchDataForEdit(nis) {
+//     fetch('http://127.0.0.1:8000/api/posts/' + nis) // Replace with your API endpoint
+//         .then(response => response.json())
+//         .then(data => {
+//             if (data.success === true) {
+//                 populateEditForm(data);
+//                 $('#editModal').modal('show');
+//             } else {
+//                 console.error("Failed to edit data.");
+//             }
+//         })
+//         .catch(error => {
+//             console.error("Error edit data:", error);
+//         });
+// }
+
+// function editData(nis) {
+//     const editedData = {
+//         nis: document.getElementById('nis').value,
+//         nama: document.getElementById('nama').value,
+//         jenis_kelamin: document.getElementById('jenis_kelamin').value,
+//         tingkat: document.getElementById('tingkat').value,
+//         jurusan: document.getElementById('jurusan').value,
+//         kd_kelas: document.getElementById('kd_kelas').value,
+//         tahun_masuk: document.getElementById('tahun_masuk').value,
+//         nik: document.getElementById('nik').value,
+//         tanggal_lahir: document.getElementById('tanggal_lahir').value,
+//         alamat: document.getElementById('alamat').value,
+//         no_tlp: document.getElementById('no_tlp').value,
+//         username: document.getElementById('username').value,
+//         password: document.getElementById('password').value
+//     };
+
+//     fetch(`http://127.0.0.1:8000/api/posts/${nis}`, { // Replace with your API endpoint for updating
+//         method: 'PUT',
+//         body: JSON.stringify(editedData),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error(`HTTP error! Status: ${response.status}`);
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             if (data.success === true) {
+//                 // console.log('Data updated successfully:', data);
+//                 $('#editModal').modal('hide'); // Hide the modal
+//                 getPosts();
+//             } else {
+//                 console.error("Failed to edit data.");
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error updating data:', error);
+//         });
+// }
+
 function deleteData(nis) {
     const confirmation = confirm("Apakah anda yakin ingin menghapus data ini?");
+    const modalBody = document.querySelector('.container-fluid');
+
     if (confirmation) {
         fetch(`http://127.0.0.1:8000/api/posts/${nis}`, {
             method: "DELETE",
@@ -164,30 +329,25 @@ function deleteData(nis) {
             .then(response => response.json())
             .then(data => {
                 if (data.success === true) {
-                    // Refresh the data after successful deletion
                     getPosts();
 
-                    // Display delete success message
                     const deleteMessage = document.createElement('div');
                     deleteMessage.classList.add('alert', 'alert-success');
                     deleteMessage.textContent = "Data berhasil dihapus!";
-                    document.body.appendChild(deleteMessage);
+                    modalBody.appendChild(deleteMessage);
 
-                    // Remove the message after a delay
                     setTimeout(() => {
                         deleteMessage.remove();
-                    }, 3000); // Remove after 3 seconds
+                    }, 1200);
                 } else {
-                    // Display delete failure message
                     const deleteMessage = document.createElement('div');
                     deleteMessage.classList.add('alert', 'alert-danger');
                     deleteMessage.textContent = "Gagal menghapus data.";
-                    document.body.appendChild(deleteMessage);
+                    modalBody.appendChild(deleteMessage);
 
-                    // Remove the message after a delay
                     setTimeout(() => {
                         deleteMessage.remove();
-                    }, 3000); // Remove after 3 seconds
+                    }, 1200);
                 }
             })
             .catch(error => {
@@ -220,38 +380,37 @@ function deleteData(nis) {
 //         });
 // }
 
-// function updateData() {
-//     if (currentEditingNis !== null) {
-//         const editForm = document.getElementById('editForm');
-//         const updatedData = {
-//             nis: editForm.nis.value,
-//             nama: editForm.nama.value,
-//             jurusan: editForm.jurusan.value,
-//             tingkat: editForm.tingkat.value,
-//             jenis_kelamin: editForm.jenis_kelamin.value,
-//             nik: editForm.nik.value,
-//             tanggal_lahir: editForm.tanggal_lahir.value,
-//             alamat: editForm.alamat.value,
-//             no_tlp: editForm.no_tlp.value
-//         };
+// function editData() {
+//     const editForm = document.getElementById('editForm');
+//     const formData = new FormData(editForm);
 
-//         fetch(`http://127.0.0.1:8000/api/posts/${currentEditingNis}`, {
-//             method: 'PUT',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify(updatedData),
+//     const nis = formData.get('nis');
+
+//     fetch(`http://127.0.0.1:8000/api/posts/${nis}`, {
+//         method: 'PUT',
+//         body: formData, // Menggunakan FormData langsung sebagai body
+//     })
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error(`HTTP error! Status: ${response.status}`);
+//             }
+//             return response.json();
 //         })
-//             .then((res) => res.json())
-//             .then((data) => {
-//                 // Proses hasil respon dari API setelah pembaruan berhasil
-//                 console.log(data);
-//                 // Tutup modal
-//                 $('#editModal').modal('hide');
-//                 // Lakukan pembaruan tampilan sesuai dengan respons dari API
-//             });
-//     }
+//         .then(data => {
+//             if (data.success === true) {
+//                 // console.log('Data updated successfully:', data);
+//                 $('#editModal').modal('hide'); // Sembunyikan modal
+//                 getPosts(); // Perbarui tampilan data
+//             } else {
+//                 console.error("Failed to edit data.");
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error updating data:', error);
+//         });
 // }
+
+
 
 
 // function getPosts() {
